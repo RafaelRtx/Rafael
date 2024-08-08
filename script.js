27df04ca72b6bba214744d622ce6611b3053d02b
rafael oliveira <rafazel@outlook.com>
2024-08-08 13:31:40 -0300

    feat: add comments and changed change badge animation timer

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
+//Selecionar as badges de decoração (HTML, CSS, JavaScript, TypeScript, NodeJs, SQL)
 const badges = document.querySelectorAll('.sobre-conteudo span')
 
+//código simples para criar uma animação nas "badges" ao carregar a página pela 1º vez
 function changeBadgeColors (){
   badges.forEach((element, index) =>{
     setTimeout(
       function addClass(){
         element.classList.add('animate')
         badges[index-1].classList.remove('animate')
-      }, index * 500
+      }, index * 1000
     )
   })
 }
